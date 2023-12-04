8c73ce0ca3fe59cb02e203cfc11786582919d1a6
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-12-04 18:01:52 +0100

    [UPDATE] added bonus1 and bonus 2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let employees = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnet-founder-ceo.jpg'
+        image: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
@@ -39,9 +39,35 @@ for(let i=0; i<employees.length; i++){
         console.log(employees[i][key]);
 
         //MILESTONE 2
-        let container = document.getElementById('content');
-        container.innerHTML += `<li>${employees[i][key]}</li>`;
+        /* let container = document.getElementById('content');
+        container.innerHTML += `<li>${employees[i][key]}</li>`; */
 
     }
 }
+let container_card = document.getElementById('cards');
+container_card.classList.add('row')
+//BONUS 1
+for(let i=0; i<employees.length; i++){
+    
+    let image = employees[i].image;
+    image = `<img src="./img/${employees[i].image}" alt="image"></img>`
+    console.log(image);
+    
+    //BONUS 2
+    let col = document.createElement('div');
+    col.classList.add('col-4');
+    col.classList.add('py-3')
+
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    let name = `<h2>${employees[i].name}</h2>`;
+    let role = `<h2>${employees[i].role}</h2>`;
+    card.innerHTML += image + '<br>' + name + '<br>' + role;
+
+    col.appendChild(card);
+    container_card.appendChild(col);
+
+}
+
 
