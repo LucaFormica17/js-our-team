d5aaebf9b925c7dc5176ff63a1ccb63574207b09
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-12-04 16:23:10 +0100

    [UPDATE] milestone 2

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
+//MILESTONE 0
 let employees = [
     {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        image: 'wayne-barnet-founder-ceo.jpg'
+        'name' : 'Wayne Barnett',
+        'role' : 'Founder & CEO',
+        'image' : 'wayne-barnet-founder-ceo.jpg'
     },
     {
-        name: 'Angela Caroll',
-        role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg'
+        'name' : 'Angela Caroll',
+        'role' : 'Chief Editor',
+        'image' : 'angela-caroll-chief-editor.jpg'
     },
     {
-        name: 'Walter Gordon',
-        role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg'
+        'name' : 'Walter Gordon',
+        'role' : 'Office Manager',
+        'image' : 'walter-gordon-office-manager.jpg'
     },
     {
-        name: 'Angela Lopez',
-        role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpg'
+        'name' : 'Angela Lopez',
+        'role' : 'Social Media Manager',
+        'image' : 'angela-lopez-social-media-manager.jpg'
     },
     {
-        name: 'Scott Estrada',
-        role: 'Developer',
-        image: 'scott-estrada-developer.jpg'
+        'name' : 'Scott Estrada',
+        'role' : 'Developer',
+        'image' : 'scott-estrada-developer.jpg'
     },
     {
-        name: 'Barbara Ramos',
-        role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg'
+        'name' : 'Barbara Ramos',
+        'role' : 'Graphic Designer',
+        'image' : 'barbara-ramos-graphic-designer.jpg'
     }
 ];
+
+//MILESTONE 1
+//dichiaro ciclo per elementi dell'array
+for(let i=0; i<employees.length; i++){
+    for(let key in employees[i]){
+        console.log(employees[i][key]);
+    }
+}
